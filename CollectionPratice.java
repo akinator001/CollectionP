e52b205b3c4fa0ec203522ae2bf31b022115200d
akinator001 <aakashgandhi1279@gmail.com>
2020-10-01 22:12:50 +0530

    [aakash] Added uc1

--- CollectionPratice.java
+++ CollectionPratice.java
@@ -3,7 +3,15 @@ package com.cp.collectionpratice;
 import java.util.Stack;
 
 public class CollectionPratice {
-
+	
+	public void stkp() {
+		Stack<Integer> stk = new Stack<Integer>();
+		stk.push(1);
+		stk.push(2);
+		stk.push(3);
+		stk.push(4);
+		System.out.println(stk);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("welcome master");
