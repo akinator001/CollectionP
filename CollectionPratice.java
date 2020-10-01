8243401f696d6a1bb7e55748f88386e0ae08e0b4
akinator001 <aakashgandhi1279@gmail.com>
2020-10-01 22:08:24 +0530

    [aakash]welcome master

---
+++ CollectionPratice.java
@@ -0,0 +1,12 @@
+package com.cp.collectionpratice;
+
+import java.util.Stack;
+
+public class CollectionPratice {
+
+
+	public static void main(String[] args) {
+		System.out.println("welcome master");
+	}
+
+}
